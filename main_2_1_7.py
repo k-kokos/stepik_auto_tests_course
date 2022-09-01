78c5e164878a5ae95535e44a848ff372df0ba67c
k-kokos <konstantin.kos0708@gmail.com>
2022-09-01 15:51:47 +0400

    Выбор radiobutton и checkbox

---
+++ main_2_1_7.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+
+try:
+    link = "http://suninjuly.github.io/get_attribute.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element(By.CSS_SELECTOR, '#treasure')
+    x = x_element.get_attribute("valuex")
+    y = calc(x)
+
+    input1 = browser.find_element(By.CSS_SELECTOR, "#answer")
+    input1.send_keys(y)
+
+    chois1 = browser.find_element(By.CSS_SELECTOR, '#robotCheckbox')
+    chois1.click()
+
+    chois2 = browser.find_element(By.CSS_SELECTOR, '#robotsRule')
+    chois2.click()
+
+    # # Отправляем заполненную форму
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
